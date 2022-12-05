fcd0ff49049f6a291717d421889ad3ff1929fc41
ThomasBoom89 <51998416+ThomasBoom89@users.noreply.github.com>
2022-12-05 22:20:27 +0100

    Camera: Zoom out when game speed increases

--- scene/CameraFix.cs
+++ scene/CameraFix.cs
@@ -9,19 +9,36 @@ public partial class CameraFix : Camera2D
 
     private Player _player;
 
+    private Game _game;
+    private float _calcQuotient;
+
     public override void _Ready()
     {
         _background = GetNode<Sprite2D>("../Background");
         _player = GetNode<Player>("../Player");
+        _game = GetNode<Game>("/root/World");
+        _calcQuotient = (float)((Game.MaxWorldSpeed - Game.MinWorldSpeed) * 0.5);
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        Position = new Vector2(_player.Position.x, Math.Min(_player.Position.y, LimitBottom));
+        if (_player.Active)
+        {
+            Position = new Vector2(_player.Position.x, Math.Min(_player.Position.y, LimitBottom));
 
-        _background.GlobalPosition = new Vector2(
-            Position.x,
-            Math.Min(_player.Position.y, (LimitBottom - _background.Texture.GetHeight() / 2))
-        );
+            _background.GlobalPosition = new Vector2(
+                Position.x,
+                Math.Min(_player.Position.y, (LimitBottom - _background.Texture.GetHeight() / 2))
+            );
+        }
+
+        if (_game.WorldSpeed <= Game.MaxWorldSpeed)
+        {
+            float offset = (_game.WorldSpeed - Game.MinWorldSpeed) / _calcQuotient;
+            float scale = (float)(1.05 + offset);
+            float zoom = (float)(1.5 - offset);
+            _background.Scale = new Vector2(scale, scale);
+            Zoom = new Vector2(zoom, zoom);
+        }
     }
 }
